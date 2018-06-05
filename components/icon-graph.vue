e0bec3a3b9355f7aaaa0ab820972b43bba5da527
7korobi <7korobi@gmail.com>
2018-06-05 15:29:03 +0900

    refactoring.

--- components/icon-graph.vue
+++ components/icon-graph.vue
@@ -1,5 +1,7 @@
 <script lang="coffee">
 dagre = require "dagre"
+{ Query } = require "~/plugins/memory-record"
+{ url } = require "~/config/live.yml"
 
 regexp_join = (regex, ...names) ->
   { flags, source } = regex
@@ -12,69 +14,65 @@ regexp_join = (regex, ...names) ->
   new RegExp source, flags
 
 syntax =
-  edges: /^( *)((\w+)?(?: *_arrow_ *\w+)+) *(?:_comment_)?(?:_eol_)?/
-  node:  /^( *)(\w+) *(?:_comment_)?(?:_eol_)?/
+  edges: /^( *)((\w+)?(?: *_arrow_ *\w+)+) *(?:_comment_)?(?:_eol_)/
+  nodes: /^( *)((?:\w| )+)(?:_comment_)?(?:_eol_)/
   newline: /^ *\n|^ +$/
 
-  _arrow_: /(-|=|\.)+(>|X|x)?/
+  _arrow_: /(-+|=+|\.+)(>|X|x|O|o)?/
   _comment_: /: *(.*) */
   _eol_: / *(?:\n|$)/
 
-syntax.node  = regexp_join syntax.node,  '_arrow_', '_comment_', '_eol_'
+syntax.nodes = regexp_join syntax.nodes, '_arrow_', '_comment_', '_eol_'
 syntax.edges = regexp_join syntax.edges, '_arrow_', '_comment_', '_eol_'
-
-console.log syntax
 
 class Render
   constructor: (@graph)->
   newline: ->
   edge: (v, w, line, arrow, label)->
-    console.log arguments
+    weight = line.length
     arrow =
       switch arrow
-        when '>'
+        when '>', ')'
           'url(#svg-marker-arrow)'
+        when 'O', 'o'
+          'url(#svg-marker-circle)'
         when 'X', 'x'
           'url(#svg-marker-cross)'
         else
           null
     line =
-      switch line
+      switch line[0]
         when '='
           'wide'
         when '-'
           'solid'
         when '.'
           'dotted'
+        else
+          'hide'
 
-    if label
-      @graph.setEdge v, w,
-        key: [v,w].join(",")
-        minlen: 1
-        weight: 1
-        arrow: arrow
-        class: line
-        label: label
-        labelpos: 'c'
-        labeloffset: 10
-        width:       20 * label.length 
-        height:      20
-    else
-      @graph.setEdge v, w,
-        key: [v,w].join(",")
-        minlen: 1
-        weight: 1
-        arrow: arrow
-        class: line
+    label ?= "   "
+    @graph.setEdge v, w,
+      key: [v,w].join(",")
+      minlen: 1
+      weight: weight
+      arrow: arrow
+      class: line
+      label: label
+      labelpos: 'c'
+      width:  25 * label.length 
+      height: 30
+      rx:      5
+      ry:      5
 
   icon: (v, label)->
     @graph.setNode v,
-      key: v
       label: label
       class: 'icon'
-      href: "https://api-project-54633717694.firebaseapp.com/images/portrate/#{v}.jpg"
       width:  100
       height: 140
+      rx:      10
+      ry:      10
   
   cluster: (v, w, label)->
     @graph.setNode w,
@@ -109,6 +107,7 @@ parse = (render, src)->
     if cap = syntax.edges.exec src
       [ all, depth, edges, v, $, $, label ] = cap
       src = src[all.length ..]
+      # console.log "edges", cap
       edges = edges
       .split syntax._arrow_
       .map (s)-> s?.trim()
@@ -127,15 +126,21 @@ parse = (render, src)->
           render.edge v, w, line, arrow, label
       continue
 
-    if cap = syntax.node.exec src
-      [ all, depth, v, label ] = cap
+    if cap = syntax.nodes.exec src
+      [ all, depth, nodes, label ] = cap
       src = src[all.length ..]
-      # console.log "node", cap
-      render.icon v, label
+      # console.log "nodes", cap
+      nodes = nodes
+      .trim()
+      .split(/ +/)
+      for v, idx in nodes
+        render.icon v, label
+        if label
+          render.edge v, v, "", "", label
 
-      if parent = find_parent v, depth
-        { label } = render.graph.node parent
-        render.cluster v, parent, label
+        if parent = find_parent v, depth
+          { label } = render.graph.node parent
+          render.cluster v, parent, label
 
       continue
 
@@ -152,12 +157,12 @@ init = ->
     # ranker: 'network-simplex'
     # ranker: 'tight-tree'
     ranker: 'longest-path'
-    rankdir: 'BT' # TB / BT / LR / RL
-    nodesep: 20
-    ranksep: 90
+    rankdir: 'RL' # TB / BT / LR / RL
+    nodesep: 10
+    ranksep: 10
     edgesep: 10
-    marginx:  0
-    marginy:  0
+    marginx:  3
+    marginy:  3
 
 module.exports =
   props: ["value"]
@@ -170,8 +175,56 @@ module.exports =
 
   computed:
     root:  -> @graph.graph()
-    nodes: -> @graph.nodes().map (key)=> @graph.node key
-    edges: -> @graph.edges().map (key)=> @graph.edge key
+    
+    edge_paths: ->
+      @graph.edges().map (key)=>
+        o = @graph.edge key
+        o?.points and Object.assign {}, o,
+          key: "path-" + key
+          "marker-end": o.arrow
+          d: @path_d o.points
+
+    edge_rects: ->
+      @graph.edges().map (key)=>
+        o = @graph.edge key
+        o?.label?.trim() and Object.assign {}, o,
+          key: "labelrect-" + key
+          x: o.x - o.width  * 0.5
+          y: o.y - o.height * 0.7
+
+    edge_labels: ->
+      @graph.edges().map (key)=>
+        o = @graph.edge key
+        o?.label and Object.assign {}, o,
+          key: "text-" + key
+
+    node_images: ->
+      @graph.nodes()
+      .map (key)=>
+        o = @graph.node key
+        if Query.faces.find(key)
+          href = "#{url.assets}/images/portrate/#{ key }.jpg"
+        o and href and
+          key: "image-" + key
+          x: o.x - o.width  / 2 + 5
+          y: o.y - o.height / 2 + 5
+          width:  o.width  - 10
+          height: o.height - 10
+          href: href
+
+    node_rects: ->
+      @graph.nodes()
+      .map (key)=>
+        o = @graph.node key
+        o and
+          key: "rect-" + key
+          rx: o.rx
+          ry: o.ry
+          x: o.x - o.width  / 2
+          y: o.y - o.height / 2
+          width:  o.width
+          height: o.height
+
     view_box: -> "0 0 #{@root.width} #{@root.height}"
 
     graph: ->
@@ -181,6 +234,7 @@ module.exports =
         parse r, @value
         dagre.layout r.graph
       catch e
+        console.warn(e)
         g = init()
         g.setNode 'error',
         label: e.message
@@ -261,24 +315,24 @@ n "div",
   > image
     transition: x .5s, y .5s
 .edges-move:not(.edges-leave-active)
-  transition: d .5s   
+  transition: d .5s
 
 </style>
 <template lang="pug">
 svg(:style="`max-width: 100%; width: ${root.width}px;`" :viewBox="view_box")
+  marker.edgePath#svg-marker-circle(viewBox="0 0 10 10" markerUnits="userSpaceOnUse" markerWidth="20" markerHeight="20" refX="2" refY="5" orient="auto")
+    circle(cx="5" cy="5" r="4")
   marker.edgePath#svg-marker-arrow(viewBox="0 0 10 10" markerUnits="userSpaceOnUse" markerWidth="20" markerHeight="20" refX="3" refY="5" orient="auto")
     path.path(d="M0,0 L10,5 L0,10 z")
   marker.edgePath#svg-marker-cross(viewBox="0 0 10 10" markerUnits="userSpaceOnUse" markerWidth="20" markerHeight="20" refX="5" refY="5" orient="0")
     path.path(d="M0,0 L10,10 M0,10 L10,0 z")
   transition-group(tag="g" name="nodes")
-    rect(v-for="o in nodes" v-if="o" :class="o.class" :key="'rect-' + o.key" :x="o.x - o.width / 2" :y="o.y - o.height / 2" :width="o.width" :height="o.height" rx="10" ry="10")
-    image(v-for="o in nodes" v-if="o && o.href" :class="o.class" :key="'img-' + o.key" :x="o.x - o.width / 2 + 5" :y="o.y - o.height / 2 + 5" :width="o.width - 10" :height="o.height - 10" :xlink:href="o.href")
+    rect( v-for="o in node_rects"  v-if="o" v-bind="o")
+    image(v-for="o in node_images" v-if="o" v-bind="o")
   transition-group.edgePath(tag="g" name="edges")
-    path.path(v-for="o in edges" v-if="o.points" :class="o.class" :key="o.key" :d="path_d(o.points)" :marker-end="o.arrow")
-  g
-    text.messageText(v-for="o in nodes" v-if="o && o.label" :x="o.x - o.width / 2" :y="20 + o.y + o.height / 2" )
-      | {{ o.label }}
-    text.messageText(v-for="o in edges" v-if="o && o.label" :x="o.x - o.width / 2" :y="o.y + o.height / 2" )
+    path.path(v-for="o in edge_paths" fill="none" v-if="o" v-bind="o")
+    rect.path(v-for="o in edge_rects" v-if="o" v-bind="o")
+    text.messageText(v-for="o in edge_labels" v-if="o" v-bind="o")
       | {{ o.label }}
 </template>
 
