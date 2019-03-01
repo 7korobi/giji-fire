2c4b2839d305baf2df3424fcba812dadaf1aa344
7korobi <7korobi@BA-1810001-MAC.local>
2019-03-01 16:44:14 +0900

    jest for mac.

--- config/jest.js
+++ config/jest.js
@@ -1,27 +1,18 @@
-const { is_windows } = require("./os")
+const { is_windows } = require("./os");
 
-let testRegex = "//__tests__//.*spec.coffee$"
-if ( is_windows ) { testRegex = "\\\\__tests__\\\\.*spec.coffee$" }
-
+let testRegex = "/__tests__/.*spec.coffee$";
+if (is_windows) {
+  testRegex = "\\\\__tests__\\\\.*spec.coffee$";
+}
 
 module.exports = {
   testRegex,
-  moduleFileExtensions: [
-    "coffee",
-    "js",
-    "json",
-    "vue",
-    "pug",
-    "yml"
-  ],
+  moduleFileExtensions: ["coffee", "js", "json", "vue", "pug", "yml"],
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/$1",
     "^@/(.*)$": "<rootDir>/$1"
   },
-  moduleDirectories: [
-    "node_modules",
-    "."
-  ],
+  moduleDirectories: ["node_modules", "."],
   transform: {
     "^.+\\.coffee$": "<rootDir>/config/coffee-jest.js",
     "^.+\\.js$": "babel-jest",
@@ -29,4 +20,4 @@ module.exports = {
     "^.+\\.pug$": "pug-jest",
     "^.+\\.vue$": "vue-jest"
   }
-}
+};
